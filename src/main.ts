de30b637ffdb621d76de9e82e933679cbd7be2ce
Rifaut Alexis <ralexis149@gmail.com>
2023-06-28 21:47:16 +0200

    Install and configure ngx-markdown

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { routes } from './app/app-routing';
 import { bootstrapApplication } from '@angular/platform-browser';
@@ -6,12 +5,14 @@ import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
+import { MarkdownModule } from 'ngx-markdown';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideAnimations(),
     importProvidersFrom(
-      RouterModule.forRoot(routes)
+      RouterModule.forRoot(routes),
+      MarkdownModule.forRoot(),
     ),
   ],
 }).catch((err) => console.error(err));
