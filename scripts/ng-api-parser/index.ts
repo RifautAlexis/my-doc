d7842229a6be7e07c65cc46be0d308fbda823314
Rifaut Alexis <ralexis149@gmail.com>
2023-12-13 22:10:03 +0100

    Remove ng-api-parser to build a package itself

--- scripts/ng-api-parser/index.ts
+++
@@ -1,9 +0,0 @@
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
-import { generateCommand } from './commands/generate';
-
-yargs(hideBin(process.argv))
-  .scriptName('ng-api-parser')
-  .usage('$0 <cmd> [args]')
-  .command(generateCommand)
-  .help().argv;
